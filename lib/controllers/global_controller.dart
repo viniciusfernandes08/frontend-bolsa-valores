5a19562f7b1fdda444211a2d8910e59b21e4110b
vinicius <viniciusfernandesoliveira8@gmail.com>
2025-12-13 15:27:20 -0300

    feat: add global controller

---
+++ lib/controllers/global_controller.dart
@@ -0,0 +1,11 @@
+import 'package:frontend_invest/models/home_model.dart';
+
+class GlobalController {
+  HomeModel? _homeData;
+
+  HomeModel setHomeData(HomeModel homeData) => _homeData = homeData;
+
+  HomeModel get homeData =>
+      _homeData ??
+      HomeModel(name: '', symbol: '', price: 0.0, change: 0.0, datetime: '');
+}
